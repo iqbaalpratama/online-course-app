2c7b72918a7bb9ccd297687cc2a0f312a9cfd6e6
iqbaalpratama <iqbaalpratama23@gmail.com>
2021-09-03 23:29:13 +0700

    add service-course

---
+++ service-course/app/Models/Course.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+    protected $table = 'courses';
+    protected $fillable = [
+        'name', 'certificate', 'thumbnail', 'type', 'status',
+        'price', 'level', 'description', 'mentor_id'
+    ];
+
+    public function mentor()
+    {
+        return $this->belongsTo('Mentor');
+    }
+    public function chapters()
+    {
+        return $this->hasMany('Chapter')->orderBy('id', 'ASC');
+    }
+    public function images()
+    {
+        return $this->hasMany('Images');
+    }
+
+}
