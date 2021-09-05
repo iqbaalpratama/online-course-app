6595e0656b0fd4111a0697ad656ee3f82250c318
iqbaalpratama <iqbaalpratama23@gmail.com>
2021-09-05 10:20:38 +0700

    api image course complete

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ImageCourseController;
 use App\Http\Controllers\LessonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -40,3 +41,6 @@ Route::get('/lessons/{id}', [LessonController::class, 'show']);
 Route::post('/lessons', [LessonController::class, 'create']);
 Route::put('/lessons/{id}', [LessonController::class, 'update']);
 Route::delete('/lessons/{id}', [LessonController::class, 'destroy']);
+
+Route::post('/image-courses', [ImageCourseController::class, 'create']);
+Route::delete('/image-courses/{id}', [ImageCourseController::class, 'destroy']);
