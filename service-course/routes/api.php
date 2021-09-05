20039ebba771d768b3c66a0a06312ca8b31b1a93
iqbaalpratama <iqbaalpratama23@gmail.com>
2021-09-05 09:27:39 +0700

    crud api lesson complete

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LessonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MentorController;
@@ -29,7 +30,13 @@ Route::put('/courses/{id}', [CourseController::class, 'update']);
 Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
 
 Route::get('/chapters', [ChapterController::class, 'index']);
-Route::get('/cjapters/{id}', [ChapterController::class, 'show']);
+Route::get('/chapters/{id}', [ChapterController::class, 'show']);
 Route::post('/chapters', [ChapterController::class, 'create']);
 Route::put('/chapters/{id}', [ChapterController::class, 'update']);
 Route::delete('/chapters/{id}', [ChapterController::class, 'destroy']);
+
+Route::get('/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'show']);
+Route::post('/lessons', [LessonController::class, 'create']);
+Route::put('/lessons/{id}', [LessonController::class, 'update']);
+Route::delete('/lessons/{id}', [LessonController::class, 'destroy']);
